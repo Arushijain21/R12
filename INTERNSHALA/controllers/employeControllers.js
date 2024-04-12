4ce33f1dfdb455c2e932d7483276de81c1d23962
Aarushi Jain <princessaaru2101@gmail.com>
2024-04-12 14:39:17 +0530

    changes

--- INTERNSHALA/controllers/employeControllers.js
+++ INTERNSHALA/controllers/employeControllers.js
@@ -188,5 +188,9 @@ exports.readjobs = catchAsyncErrors(async (req, res, next) => {
         res.status(200).json({ success: true,jobs });
     });
    
+exports.readsinglejobs = catchAsyncErrors(async (req, res, next) => {
+        const job = await Job.findById(req.params.id).exec();
+         res.status(200).json({ success: true, job });
+    });    
 
 
